930fc20acf1e3615cfeb2b9667002edf08ec99a5
Sydney <sydvand@Sydneys-Air.attlocal.net>
2022-03-02 09:02:58 -0600

    build tools

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,6 +1,6 @@
 package com.galvanize;
 
-public class application {
+public class Application {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
---
+++ src/main/java/com/galvanize/program.java
@@ -0,0 +1,7 @@
+class program {
+
+    public static void main(String[] args) {
+
+        System.out.println("It Works!!");
+    }
+}
